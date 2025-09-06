7e34bd026e2539dba2a61ce9b7b8022dfab6c1db
Aditya Dutt Pandey <adityapandeyadp@gmail.com>
2025-09-06 20:01:09 +0530

    tesrt

---
+++ test/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello, World!'
+    });
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+export default app;
